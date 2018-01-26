c99ad0e54d0c171ea650016641885d2082e8e357
enrro <edgarbg585@gmail.com>
2018-01-25 20:32:41 -0600

    refactor and made a integration test.

--- practicas/practica2/operationstest.c
+++ practicas/practica2/operationstest.c
@@ -1,37 +1,42 @@
 #include <stdio.h>
 #include "operations.h"
 
+void printTestMessage(int, char *, char *);
+
 int main()
 {
     //test addition.2
     int a = 2147483645;
     int b = 1;
-    int result = add(a,b);
+    int result = add(a, b);
     int expected = a + b;
-
-    if( result == expected)
-    {
-        printf("%s\n", "addition test passed");
-    }
-    else
-    {
-        printf("%s\n", "addition test failed");
-    }
+    printTestMessage(result == expected,
+                     "addition test passed",
+                     "addition test failed");
 
     //test substraction
     int d = -2147483645;
     int f = 1;
     int resultSubstraction = substraction(d, f);
     int expectedSubstraction = d - f;
+    printTestMessage(resultSubstraction == expectedSubstraction,
+                     "Substraction test passed",
+                     "Substraction test failed");
 
-    if( resultSubstraction == expectedSubstraction)
+
+    return 0;
+}
+
+void printTestMessage(int testResult,
+                      char *passedMessage,
+                      char *failedMessage)
+{
+    if (testResult)
     {
-        printf("%s\n", "Substraction test passed");
+        printf("%s\n", passedMessage);
     }
     else
     {
-        printf("%s\n", "Substraction test failed");
+        printf("%s\n", failedMessage);
     }
-
-    return 0;
 }
